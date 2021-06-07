ee6bfa40d6471d341a6f84e8f619ea07cd4d9181
c0mPlicaTedd <shirothegamer69@gmail.com>
2021-06-07 11:09:52 +0530

    Updater updated

--- stocks/dbUpdater.py
+++ stocks/dbUpdater.py
@@ -6,40 +6,13 @@ from selenium import webdriver
 from dbHandler import update
 from datetime import datetime
 from datetime import date
-from datetime import time
+
 
 option = webdriver.ChromeOptions()
 option.add_argument('headless')
 browser = webdriver.Chrome('C:\\Users\Jash\Downloads\chromedriver_win32\chromedriver.exe',options=option)
 
 
-<<<<<<< Updated upstream
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-opening_time = time(9,0,0)
-closing_time = time(15,30,0)
-day = date.today().strftime("%A")
-
-
-if (str(current_time)>str(opening_time) and str(current_time)<str(closing_time) and day != "Saturday" and day!="Sunday" ):
-    f= open("C:\\Users\Jash\DummyStocks\stocks\stocknames.txt",'r')
-    for line in f: #automated updates
-        shortname = line
-        url = "https://www.google.com/search?q=NSE:"+shortname
-        browser.get(url)
-        price = browser.find_element_by_xpath("/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/g-card-section/div/g-card-section/div[2]/div[1]/span[1]/span/span[1]").text
-        price = price.replace(",",'')
-        price = float(price)
-        movement = browser.find_element_by_xpath("/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/g-card-section/div/g-card-section/div[2]/div[1]/span[2]/span[1]").text 
-        movement = movement.replace(",","")
-        percentage = browser.find_element_by_xpath("/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/g-card-section/div/g-card-section/div[2]/div[1]/span[2]/span[2]/span[1]").text
-        percentage = percentage.replace("(","")
-        percentage = percentage.replace(")","")
-        
-        update(shortname.rstrip("\n"),price,movement,percentage)
-        #c = c+1 (c undefined)
-        #print(c)
-=======
 current_time = datetime.now()
 opening_time = current_time.replace(hour=9, minute=0, second=0, microsecond=0)
 closing_time = current_time.replace(hour=15, minute=30, second=0, microsecond=0)
@@ -66,7 +39,6 @@ if (str(current_time)>str(opening_time) and str(current_time)<str(closing_time) 
             print(c)
     except:
         browser.quit()
->>>>>>> Stashed changes
     f.close()
     browser.quit()
 
